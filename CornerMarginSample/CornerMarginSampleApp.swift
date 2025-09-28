c046d84f1ff99601bf6a12e7a2b8a9686985425d
juniperphoton <dengweichao@hotmail.com>
2025-09-28 15:20:02 +0800

    Switch to UIScene based lifecycle

--- CornerMarginSample/CornerMarginSampleApp.swift
+++ CornerMarginSample/CornerMarginSampleApp.swift
@@ -7,7 +7,44 @@
 
 import SwiftUI
 
+class UISceneDelegation: NSObject, UIWindowSceneDelegate {
+    var window: UIWindow?
+    
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(
+                rootView: ContentView().injectSafeAreaInsetsDetector()
+            )
+            window.makeKeyAndVisible()
+            self.window = window
+        }
+    }
+    
+    func preferredWindowingControlStyle(
+        for windowScene: UIWindowScene
+    ) -> UIWindowScene.WindowingControlStyle {
+        .minimal
+    }
+}
+
 @main
+class UIAppDelegataion: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        configurationForConnecting connectingSceneSession: UISceneSession,
+        options: UIScene.ConnectionOptions
+    ) -> UISceneConfiguration {
+        let sceneConfig = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+        sceneConfig.delegateClass = UISceneDelegation.self
+        return sceneConfig
+    }
+}
+
 struct CornerMarginSampleApp: App {
     var body: some Scene {
         WindowGroup {
