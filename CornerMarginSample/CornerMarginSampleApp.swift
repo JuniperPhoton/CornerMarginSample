e38ece701bc5b2610ea69720de951cf352c67a2b
juniperphoton <dengweichao@hotmail.com>
2025-09-28 12:08:28 +0800

    Initial Commit

---
+++ CornerMarginSample/CornerMarginSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CornerMarginSampleApp.swift
+//  CornerMarginSample
+//
+//  Created by juniperphoton on 9/28/25.
+//
+
+import SwiftUI
+
+@main
+struct CornerMarginSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
